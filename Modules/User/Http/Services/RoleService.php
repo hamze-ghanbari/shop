48595da1e2da8cb2de8c1252dc400d8512fed5b8
hamze ghanbari <infogh1000@gmail.com>
2023-07-16 00:51:19 +0430

    use RoleReapository in RoleService

--- Modules/User/Http/Services/RoleService.php
+++ Modules/User/Http/Services/RoleService.php
@@ -5,34 +5,40 @@ namespace Modules\User\Http\Services;
 use Illuminate\Http\Request;
 use Modules\User\Entities\Permission;
 use Modules\User\Entities\Role;
+use Modules\User\Repository\RoleRepositoryInterface;
 
 class RoleService
 {
+
+    public function __construct(
+        public RoleRepositoryInterface $roleRepository
+    ){}
+
     public function getSearchRoles(Request $request)
     {
-        return Role::with('permissions')->search($request->query('search'))->paginate(15);
+        return $this->roleRepository->with('permissions')->search($request->query('search'))->paginate(15);
     }
 
     public function createRole(Request $request)
     {
         $status = $request->has('status') ? $request->input('status') : 0;
-        Role::create($request->fields(attributes: ['status' => $status]));
+        $this->roleRepository->create($request->fields(attributes: ['status' => $status]));
     }
 
     public function updateRole(Request $request, Role $role)
     {
         $status = $request->has('status') ? $request->input('status') : 0;
-        return $role->update($request->fields(attributes: ['status' => $status]));
+        return $this->roleRepository->update($request->fields(attributes: ['status' => $status]), $role->id);
     }
 
     public function updateRoleStatus(Role $role, $status){
-        return $role->update([
+        return $this->roleRepository->update([
             'status' => $status
-        ]);
+        ], $role->id);
     }
 
-    public function deleteRole(Role $role){
-       return $role->delete();
+    public function deleteRole($id){
+       return $this->roleRepository->delete($id);
     }
 
     public function getPermissions(Role $role){
