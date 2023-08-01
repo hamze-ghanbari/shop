8a2881ee4c1b458f978f1e7f182cf552fb96c487
hamze ghanbari <infogh1000@gmail.com>
2023-08-01 22:42:22 +0430

    create category routes

--- Modules/Product/Routes/web.php
+++ Modules/Product/Routes/web.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
-Route::prefix('product')->group(function() {
-    Route::get('/', 'ProductController@index');
+Route::prefix('products')->group(function(){
+    Route::resource('categories', 'CategoryController')->except('show');
+    Route::get('categories/{category}/status/{status}', 'CategoryController@changeStatus')->name('categories.status');
 });
