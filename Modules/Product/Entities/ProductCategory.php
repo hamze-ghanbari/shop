9e0da3e6ed4f88e6f631d2cd4d47f3ab0d374899
hamze ghanbari <infogh1000@gmail.com>
2023-08-01 22:39:45 +0430

    create productCategory model

--- Modules/Product/Entities/ProductCategory.php
+++ Modules/Product/Entities/ProductCategory.php
@@ -2,14 +2,40 @@
 
 namespace Modules\Product\Entities;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class ProductCategory extends Model
 {
     use  SoftDeletes;
 
-    protected $fillable = [];
+    protected $guarded = ['id'];
 
+    public function parent()
+    {
+        return $this->belongsTo($this, 'parent_id')->with('parent');
+    }
+
+    public function children()
+    {
+        return $this->hasMany($this, 'parent_id')->with('children');
+    }
+
+    protected function slug(): Attribute{
+        return Attribute::make(
+            set: fn () =>  Str::slug($this->attributes['name'])
+        );
+    }
+
+    public function scopeSearch(Builder $query, $term = null)
+    {
+        return $query->when($term, function (Builder $query, $term) {
+            $query->where('name', 'like', "%{$term}%")
+                ->orWhere('persian_name', 'like', "%{$term}%");
+        });
+    }
 }
