820b1b970a4d258db64dc93162fcb4d0dece0cd8
hamze ghanbari <infogh1000@gmail.com>
2023-08-01 22:42:42 +0430

    change user routes

--- Modules/User/Routes/web.php
+++ Modules/User/Routes/web.php
@@ -5,15 +5,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('profile/{user}', 'UserController@show')->name('users.profile');
 Route::resource('users', 'UserController')->except('show');
-Route::get('users/{user}/role/create', 'UserController@showRoles')->name('users.role.show');
-Route::post('users/{user}/role/create', 'UserController@addRoleToUser')->name('users.role.add');
-Route::get('users/{user}/permission/create', 'UserController@showPermissions')->name('users.permission.show');
-Route::post('users/{user}/permission/create', 'UserController@addPermissionToUser')->name('users.permission.add');
-Route::post('users/{user}/download', 'UserController@download')->name('users.download');
+Route::prefix('users')->group(function(){
+Route::get('{user}/role/create', 'UserController@showRoles')->name('users.role.show');
+Route::post('{user}/role/create', 'UserController@addRoleToUser')->name('users.role.add');
+Route::get('{user}/permission/create', 'UserController@showPermissions')->name('users.permission.show');
+Route::post('{user}/permission/create', 'UserController@addPermissionToUser')->name('users.permission.add');
+Route::post('{user}/download', 'UserController@download')->name('users.download');
+Route::put('updateName/{user}', 'UserController@updateName')->name('users.name.update');
+Route::put('updatenatioanlCode/{user}', 'UserController@updatenatioanlCode')->name('users.national-code.update');
+Route::put('updateBirthDate/{user}', 'UserController@updateBirthDate')->name('users.birth-date.update');
+});
 
-Route::put('users/updateName/{user}', 'UserController@updateName')->name('users.name.update');
-Route::put('users/updatenatioanlCode/{user}', 'UserController@updatenatioanlCode')->name('users.national-code.update');
-Route::put('users/updateBirthDate/{user}', 'UserController@updateBirthDate')->name('users.birth-date.update');
 //Route::get('users', 'UserController@index')->name('user.index');
 //Route::get('users', 'UserController@index')->name('user.index');
 
